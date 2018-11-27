0a2e2acdcbb2dc6d4bbc776ce00416fea665bd58
suchgabi <32851808+suchgabi@users.noreply.github.com>
2018-11-27 00:37:46 +0000

    Updated index.js (changed the file name)

--- js/index.js
+++ js/index.js
@@ -20,4 +20,4 @@ options = {
 vrvToolkit.setOptions(options)
 
 /* Show the score */
-showScore('score', 'data/example.mei') // <-- CHANGE THIS TO YOUR FILE
+showScore('score', 'data/Bohemian_Rhapsody.mei') // <-- CHANGE THIS TO YOUR FILE
