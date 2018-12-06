78d971fc01b3ac08faef84cf787742d5d1338c6b
suchgabi <32851808+suchgabi@users.noreply.github.com>
2018-12-06 22:01:37 +0000

    changed verovio window size

--- js/index1.js
+++ js/index1.js
@@ -13,7 +13,7 @@ if (content) {
 var defaultHeight = document.documentElement.clientHeight - contentHeight
 var defaultWidth = document.documentElement.clientWidth - 50
 options = {
-    pageHeight: defaultHeight * 100 / zoom,
+    pageHeight: defaultHeight * 300 / zoom,
     pageWidth: defaultWidth * 100 / zoom,
     scale: zoom
 }
